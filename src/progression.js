2022267a83e6037c80dc9c98260fd87b0d52481e
oussamaelhadad <oussamaelhadad2005@gmail.com>
2026-09-17 18:01:23 +0100

    functionvaliderResultatfunctionajouterApprenantfunctionenregistrerResultat

--- src/progression.js
+++ src/progression.js
@@ -1,3 +1,5 @@
+const apprenants = require("./data")
+
 function normaliserNom(nom){
 nom = nom.trim()
 nom = nom.toLowerCase()
@@ -21,4 +23,56 @@ nouveaunom = nouveaunom.trim()
 
 return nouveaunom
 }
+
+function validerResultat(resultat){
+if(resultat.jour<1 || resultat.jour>7){
+  return "invalid informatione"
+}
+if (resultat.exercicesTermines<0 || resultat.exercicesTermines>20){
+  return "invalid informatione"
+}
+if (resultat.totalExercices!==20){
+  return "invalid informatione"
+}
+if (typeof(resultat.challengeTermine)!=="boolean"){
+return "invalid informatione"
+}
+  return true
+  
+}
+
+function ajouterApprenant(apprenants, nouveauApprenant) {
+  for(let i=0; i<apprenants.length; i++){
+    if(apprenants[i].id == nouveauApprenant.id)
+      return "id est existe déjà"
+  }
+   apprenants.push(nouveauApprenant)
+  return true
+}
+
+function enregistrerResultat(apprenants, id, resultats){
+  if(validerResultat(resultats) != true )
+      return "resultat non valide"
+ 
+  for(let i=0; i<apprenants.length; i++){
+    
+    if(apprenants[i].id == id){
+    
+      for(let j=0; j<apprenants[i].resultats.length; j++){
+      
+         if(apprenants[i].resultats[j].jour == resultats.jour){
+         apprenants[i].resultats[j] = resultats
+         return true
+      }
+         
+      }
+      apprenants[i].resultats.push(resultats)
+      return true
+      
+    }
+    
+  }
+  return "apprenants untrouvable"
+}
+
 
