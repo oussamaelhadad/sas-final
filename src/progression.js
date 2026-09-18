481ef5ee4d9e091347c1548d2e7b28106dbe11ce
oussamaelhadad <oussamaelhadad2005@gmail.com>
2026-09-18 17:58:32 +0100

    function de calculerProgression + function de filtrerParNiveau

--- src/progression.js
+++ src/progression.js
@@ -1,4 +1,5 @@
 const apprenants = require("./data")
+const prompt = require("prompt-sync")();
 
 function normaliserNom(nom){
 nom = nom.trim()
@@ -41,46 +42,145 @@ return "invalid informatione"
   
 }
 
-function ajouterApprenant(apprenants, nouveauApprenant) {
-  for(let i=0; i<apprenants.length; i++){
-    if(apprenants[i].id == nouveauApprenant.id)
-      return "id est existe déjà"
+function ajouterApprenant(apprenants) {
+  let id = prompt("Entrez l'ID : ")
+  for (let i = 0; i < apprenants.length; i++) {
+    if (apprenants[i].id == Number(id)) {
+      return "ID existe déjà";
+    }
   }
-   apprenants.push(nouveauApprenant)
-  return true
+  let nom = prompt("Entrez le nom complet : ")
+  let ville = prompt("Entrez la ville : ")
+  let nouveauApprenant = {
+    id: id,
+    nomComplet: normaliserNom(nom),
+    ville: ville,
+    resultats: []
+  };
+
+  apprenants.push(nouveauApprenant)
+  return apprenants;
 }
 
-function enregistrerResultat(apprenants, id, resultats){
-  if(validerResultat(resultats) != true )
-      return "resultat non valide"
- 
-  for(let i=0; i<apprenants.length; i++){
-    
-    if(apprenants[i].id == id){
-    
-      for(let j=0; j<apprenants[i].resultats.length; j++){
-      
-         if(apprenants[i].resultats[j].jour == resultats.jour){
-         apprenants[i].resultats[j] = resultats
-         return true
-      }
-         
-      }
-      apprenants[i].resultats.push(resultats)
-      return true
-      
-    }
-    
+function enregistrerResultat(apprenants) {
+
+  let id = Number(prompt("Entrez l'ID de l'apprenant : "));
+  let jour = Number(prompt("Entrez le jour : "));
+  let exercicesTermines = Number(prompt("Entrez le nombre d'exercices terminés : "));
+  let challenge = prompt("Challenge terminé ? true/false");
+
+  let challengeTermines = challenge.toLowerCase() == "true";
+
+  let resultats = {
+    jour: jour,
+    exercicesTermines: exercicesTermines,
+    totalExercices: 20,
+    challengeTermines: challengeTermines
+  };
+
+  if (validerResultat(resultats) != true){
+    return "resultat non valide";
   }
-  return "apprenants untrouvable"
+    
+
+  for (let i = 0; i < apprenants.length; i++) {
+
+    if (apprenants[i].id == id) {
+
+      for (let j = 0; j < apprenants[i].resultats.length; j++) {
+
+        if (apprenants[i].resultats[j].jour == jour) {
+          apprenants[i].resultats[j] = resultats;
+          return true;
+        }
+      }
+      apprenants[i].resultats.push(resultats);
+      return true;
+    }
+  }
+
+  return "apprenant introuvable";
 }
-function rechercherApprenant(apprenants, recherche){
+
+function rechercherApprenant(apprenants) {
+
+  let recherche = prompt("Entrez l'ID ou le nom de l'apprenant : ")
+
+  for (let i = 0; i < apprenants.length; i++) {
+   
+    if (apprenants[i].id === Number(recherche) ||apprenants[i].nomComplet.toLowerCase().includes(recherche.toLowerCase())){
+      return apprenants[i];
+    }
+  }
+  return "apprenant introuvable"
+}
+
+function calculerProgression (apprenant){
+  let totaleExercicestermines = 0
+  let totaleExercicesProposes = 0
+  let totaleChallengestermines = 0
+  let JournéesRenseignées = 0
+ 
+  for(let i=0; i<apprenant.resultats.length; i++){
+    totaleExercicestermines = totaleExercicestermines + apprenant.resultats[i].exercicesTermines
+
+    totaleExercicesProposes = totaleExercicesProposes + apprenant.resultats[i].totalExercices
+   
+    if(apprenant.resultats[i].challengeTermine){
+      totaleChallengestermines++
+    }
+    JournéesRenseignées++
+}
+if (apprenant.resultats.length === 0){
+    return 0;
+}
+
+let progression = (totaleExercicestermines/totaleExercicesProposes)*100
+
+if (progression >= 80){
+  niveau = "Solide"
+}
+else if (progression >= 50 && progression <= 79){
+  niveau = "En progression"
+}
+else{
+  niveau = "À renforcer"
+}
+
+return {
+  progression : progression,
+  niveau : niveau,
+  totaleExercicestermines : totaleExercicestermines,
+  totaleExercicesProposes : totaleExercicesProposes,
+  totaleChallengestermines : totaleChallengestermines,
+  JournéesRenseignées : JournéesRenseignées
+}
+}
+
+function filtrerParNiveau (apprenants){
+  let niveau = prompt("Entrez le niveau que vous recherchez : ")
+  niveau = niveau.toLowerCase()
+  let resultats = []
 
   for(let i = 0; i < apprenants.length; i++){
+    let resultat = calculerProgression(apprenants[i])
+   
+    if(resultat.niveau.toLowerCase()===(niveau)){
+      resultats.push(apprenants[i])
+    }
 
-    if (apprenants[i].nomComplet.toLowerCase().includes(recherche.toLowerCase()))
-      return apprenants[i]
   }
-  return "apprenant untrouvable"
+  return resultats
 }
+function triterParProgression (apprenants){
+ let resultat = [...apprenants]
+  
+  for(let i = 0; i < resultat.length; i++){
+    let progression1 =calculerProgression(resultat[i])
+    let progression2 = calculerProgression(resultat[i+1])
+    if(progression1.progression > progression2.progression){
 
+    }
+
+  }
+}
