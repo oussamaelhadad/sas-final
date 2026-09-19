381c50527f306d8c1e7becb61c50bd64506cc56a
oussamaelhadad <oussamaelhadad2005@gmail.com>
2026-09-19 19:47:20 +0100

    index

---
+++ src/index.js
@@ -0,0 +1,50 @@
+let choix = prompt(`
+SAS PROGRESS CONSOLE
+
+1. Afficher le tableau de bord
+2. Afficher la liste des apprenants
+3. Ajouter un apprenant
+4. Consulter un apprenant par identifiant
+5. Ajouter ou modifier le résultat d'une journée
+6. Rechercher un apprenant par nom
+7. Filtrer les apprenants par niveau
+8. Trier les apprenants par progression décroissante
+9. Trier les apprenants par ordre alphabétique
+0. Quitter
+
+Votre choix :
+`)
+if(choix == 1){
+    console.log("Afficher le tableau de bord")
+}
+else if (choix == 2){
+    console.log("Afficher la liste des apprenants")
+}
+else if (choix == 3){
+    console.log("Ajouter un apprenant")
+}
+else if (choix == 4){
+    console.log("Consulter un apprenant par identifiant")
+}
+else if (choix == 5){
+    console.log("Ajouter ou modifier le résultat d'une journée")
+}
+else if (choix ==6){
+    console.log("Rechercher un apprenant par nom")
+}
+else if (choix == 7){
+    console.log("Filtrer les apprenants par niveau")
+}
+else if (choix == 8){
+    console.log("Trier les apprenants par progression décroissante")
+}
+else if (choix == 9){
+    console.log("Trier les apprenants par ordre alphabétique")
+}
+else if (choix == 0){
+    console.log("Quitter")
+}
+switch (choix){
+    case "1" :
+
+}
